396adf3f16e820cd4278f4312a775cc1e098c3c1
Aliev Isa <takata666@mail.ru>
2023-06-06 14:47:49 +0300

    initial commit

---
+++ app/src/main/java/com/example/movielibrary/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.movielibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+/**
+ * Main activity
+ * Это главная activity приложения. Здесь происходит навигация между фрагментами.
+ * @constructor создает пустой Main activity
+ */
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController // Отвечает за навигацию между фрагментами
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        // Находим NavHostFragment и получаем NavController
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+        // Устанавливаем BottomNavigationView для NavController
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/example/movielibrary/adapters/HomeAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.movielibrary.adapters
+
+class HomeAdapter {
+}
